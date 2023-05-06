9b91ce8f223205bdfac2510db9f466700f1ba952
Hamelen <132668682+Hamelen@users.noreply.github.com>
2023-05-06 17:28:17 +0900

    Add files via upload

---
+++ Ex2_1.java
@@ -0,0 +1,15 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		//Sysout ->sop로 ctrl+space 자동완성
+		//ctrl+alt+down 행단위 복사
+		//shift +down 2번
+		//alt+ shift+ A
+
+	}
+
+}
